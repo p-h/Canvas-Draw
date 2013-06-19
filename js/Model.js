09b4449a1ec07b1fe57b4bb5daa1627eb60f7f78
Philippe Hürlimann <phihue@gmail.com>
2013-06-19 08:31:46 +0200

    besserer konstruktor und dummy models

--- js/Model.js
+++ js/Model.js
@@ -1,10 +1,12 @@
 (function() {
 	window.DrawingApp = window.DrawingApp || {}
 	$.extend(window.DrawingApp, {
-		DrawingItem : function() {
-			this.id = ""
-			this.title = ""
-			this.dataUrl = ""
-		}
+		DrawingItem : function(id, title, dataUrl) {
+			this.id = id
+			this.title = title
+			this.dataUrl = dataUrl
+		},
+		// TODO: Remove
+		DummyDrawingItems : [new DrawingItem("d5981622-ad7d-4184-88c6-f0a9ee9cc00e", "foo", null), new DrawingItem("5848c8ae-4767-49d1-a973-77089646f1f6", "bar", null), new DrawingItem("5ee372ec-d15d-4d2d-a4ea-88ab439f8485", "baz", null)]
 	})
 })()
