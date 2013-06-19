01f5ebd3e627e065e6fa7bb57b6fa35cb166aa62
Philippe Hürlimann <phihue@gmail.com>
2013-06-19 14:11:24 +0200

    updated Model property

--- js/Model.js
+++ js/Model.js
@@ -1,13 +1,12 @@
-(function() {
-	window.DrawingApp = window.DrawingApp || {}
-	$.extend(window.DrawingApp, {
-		DrawingItem : function(id, title, dataUrl) {
-			this.id = id
-			this.title = title
-			this.dataUrl = dataUrl
-		}
-	})
+window.DrawingApp = window.DrawingApp || {}
+window.DrawingApp.Model = (function() {
+	function DrawingItem(id, title, dataUrl) {
+		this.id = id
+		this.title = title
+		this.dataUrl = dataUrl
+	}
 
-	// TODO: Remove
-	window.DrawingApp.DummyDrawingItems = [new DrawingApp.DrawingItem("d5981622-ad7d-4184-88c6-f0a9ee9cc00e", "foo", null), new DrawingApp.DrawingItem("5848c8ae-4767-49d1-a973-77089646f1f6", "bar", null), new DrawingApp.DrawingItem("5ee372ec-d15d-4d2d-a4ea-88ab439f8485", "baz", null)]
+	return {
+		DrawingItem : DrawingItem
+	}
 })()
