1f7c810494844e618b7346353283b5c39cc6ee2f
Philippe Hürlimann <p@hurlimann.org>
2013-06-20 18:19:33 +0200

    create sachen Controller.js verschoben und ein test filling hinzugefügt, das zeichnen funzt aber nicht

--- js/Controller.js
+++ js/Controller.js
@@ -31,5 +31,111 @@ $(function() {
 			$.mobile.navigate("#home")
 		}
 	})
+
+	$("#create-drawing").on("pagebeforeshow", function() {
+		function save() {
+
+			//Fragt nach dem Titel des Bildes
+			var title = prompt("Titel:", "DrawingApp Zeichnung");
+			//Speichert die Canvas als DataURL
+			var dataURL = canvas.toDataURL();
+
+			var item = new DrawingApp.Model.DrawingItem(null, title, dataURL);
+			var db = DrawingApp.DB.insertDrawingItem(item);
+		}
+
+		//Canvas Element und Context holen
+		var canvas = document.getElementById('canvas');
+		var context = canvas.getContext('2d');
+
+		context.fillStyle = "green"
+		context.fillRect(0, 0, 80, 100)
+
+		//Event Listener hinzufügen. Wenn der User die Zeichenfläche
+		//berürht, startet er den touchstart Event, bewegt er den
+		//Finger, beginnt das touchmove Event, hebt er den Finger
+		//beginnt das touchend Event. Alle 3 rufen die draw Funktion auf.
+		canvas.addEventListener('touchstart', draw, false);
+		canvas.addEventListener('touchmove', draw, false);
+		canvas.addEventListener('touchend', draw, false);
+
+		//Das Zeichentool.
+		var pen = {
+			isDrawing : false, //Am Anfang ist isDrawing auf false gesetzt, da noch nicht gezeichnet wird.
+
+			//Sobald der User zu zeichnen beginnt, holt sich touchstart die Position des Fingers.
+			touchstart : function(position) {
+				context.beginPath();
+				//Der Path wird begonnen.
+				context.moveTo(position.x, position.y);
+				//Startposition wird auf die geholten Koordinaten gesetzt.
+				this.isDrawing = true;
+				//Der User zeichnet jetzt, also ist isDrawing nun true.
+			},
+
+			//Bewegt sich der Finger, müssen die neuen Koordinaten geholt werden.
+			touchmove : function(position) {
+				//Ist der User am zeichnen, wird eine Linie von den Anfangskoordinaten zu
+				//den neuen Koordinaten gezeichnet
+				if (this.isDrawing) {
+					context.lineTo(position.x, position.y);
+					context.stroke();
+				}
+			},
+			// Hebt der User den Finger werden die letzten Koordinaten geholt.
+			touchend : function(position) {
+				//war der User am zeichnen, wird der letzte Teil der Linie noch gezeichnet.
+				//Dies geschieht über einen Aufruf von touchmove. Danach wird isDrawing auf
+				//false gesetzt.
+				if (this.isDrawing) {
+					this.touchmove(position);
+					this.isDrawing = false;
+				}
+			}
+		}
+
+		//Funktion, die die Events handelt und die Koordinaten holt und
+		//an pen weiterleitet.
+		function draw(event) {
+			//Prüfen, ob nur ein Finger gerade auf dem Bildschirm ist
+			if (event.targetTouches.length == 1) {
+				//Koordinaten werden geholt
+				var position = {
+					x : event.targetTouches[0].clientX - getOffsetLeft(canvas),
+					y : event.targetTouches[0].clientY - getOffsetTop(canvas)
+				};
+
+				function getOffsetLeft(elem) {
+					var offsetLeft = 0;
+					do {
+						if (!isNaN(elem.offsetLeft)) {
+							offsetLeft += elem.offsetLeft;
+						}
+					} while( elem = elem.offsetParent );
+					return offsetLeft;
+				}
+
+				function getOffsetTop(elem) {
+					var offsetTop = 0;
+					do {
+						if (!isNaN(elem.offsetTop)) {
+							offsetTop += elem.offsetTop;
+						}
+					} while( elem = elem.offsetParent );
+					return offsetTop;
+				}
+
+				//Koordinaten werden übergeben
+				pen[event.type](position);
+			}
+
+		}
+
+		//Verhindert, dass der Device automatisch elastisch scrollt.
+		document.body.addEventListener('touchmove', function(event) {
+			event.preventDefault();
+		}, false);
+
+	})
 })
 
